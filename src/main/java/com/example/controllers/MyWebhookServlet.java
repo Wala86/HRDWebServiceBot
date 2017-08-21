7b7806d25e57db633b68355db32a9df00282ae62
Wala86 <benamorwala@yahoo.fr>
2017-08-21 17:40:41 +0200

    Update MyWebhookServlet.java

--- src/main/java/com/example/controllers/MyWebhookServlet.java
+++ src/main/java/com/example/controllers/MyWebhookServlet.java
@@ -8,6 +8,10 @@ import ai.api.model.Fulfillment;
 
 @WebServlet("/webhook")
 public class MyWebhookServlet extends AIWebhookServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Override
 	protected void doWebhook(AIWebhookRequest input, Fulfillment output) {
 		output.setDisplayText("You said: " + input.getResult().getFulfillment().getDisplayText());
