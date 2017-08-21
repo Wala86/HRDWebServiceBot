fc612d4d08f6bc828ee791449644dbaad6aad3a8
Wala86 <benamorwala@yahoo.fr>
2017-08-21 17:36:56 +0200

    Update CustomerController.java

--- src/main/java/com/example/controllers/CustomerController.java
+++ src/main/java/com/example/controllers/CustomerController.java
@@ -30,32 +30,22 @@ import ai.api.model.Fulfillment;
 @RestController
 public class CustomerController {
 
-	@RequestMapping("/resource")
-	public Map<String, Object> home() {
-		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("id", UUID.randomUUID().toString());
-		model.put("content", "Hello World");
-		return model;
-	}
-
-	// @RequestMapping(value = "/webhook", method = RequestMethod.POST)
-	// private Map<String,Object> webhook(@RequestParam("shipping-zone") String
-	// shippingZone, @RequestParam("action") String action) throws JSONException {
-	//
-	//
-	//
-	// Map<String,Object> json = new HashMap<String,Object>();
-	//
-	// json.put("speech", " The cost of shipping to is 1000 euros.");
-	// json.put("displayText", " The cost of shipping to is 1000 euros.");
-	//
-	// json.put( "source", "apiai-onlinestore-shipping");
-	// System.out.println("*************
-	// "+shippingZone+"******************"+action);
-	// return json;
-	//
-	// }
 	/*
+	 * @RequestMapping("/resource") public Map<String, Object> home() { Map<String,
+	 * Object> model = new HashMap<String, Object>(); model.put("id",
+	 * UUID.randomUUID().toString()); model.put("content", "Hello World"); return
+	 * model; }
+	 * 
+	 * // @RequestMapping(value = "/webhook", method = RequestMethod.POST) //
+	 * private Map<String,Object> webhook(@RequestParam("shipping-zone") String //
+	 * shippingZone, @RequestParam("action") String action) throws JSONException {
+	 * // // // // Map<String,Object> json = new HashMap<String,Object>(); // //
+	 * json.put("speech", " The cost of shipping to is 1000 euros."); //
+	 * json.put("displayText", " The cost of shipping to is 1000 euros."); // //
+	 * json.put( "source", "apiai-onlinestore-shipping"); //
+	 * System.out.println("************* //
+	 * "+shippingZone+"******************"+action); // return json; // // } /*
+	 * 
 	 * @RequestMapping(value = "/webhook", method = RequestMethod.POST)
 	 * private @ResponseBody Map<String,Object> webhook(@RequestBody
 	 * Map<String,Object> obj) throws JSONException {
@@ -95,14 +85,5 @@ public class CustomerController {
 	 * 
 	 * }
 	 */
-
-	@WebServlet("/webhook")
-	public class MyWebhookServlet extends AIWebhookServlet {
-		@Override
-		protected void doWebhook(AIWebhookRequest input, Fulfillment output) {
-			output.setDisplayText("You said: " + input.getResult().getFulfillment().getDisplayText());
-			System.out.println("-----------------You said:---------------------" + input.getResult().getFulfillment().getDisplayText());
-		}
-	}
 
 }
