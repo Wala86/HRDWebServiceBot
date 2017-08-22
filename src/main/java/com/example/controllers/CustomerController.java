91b86d107bb70fe51f0f3fe55e668a38827f44c3
Wala86 <benamorwala@yahoo.fr>
2017-08-22 12:16:18 +0200

    Update CustomerController.java

--- src/main/java/com/example/controllers/CustomerController.java
+++ src/main/java/com/example/controllers/CustomerController.java
@@ -33,7 +33,7 @@ public class CustomerController {
 		json.put("speech", " The cost of shipping to  is  1000 euros.");
 		json.put("displayText", " The cost of shipping to  is  1000 euros.");
 		json.put("source", "apiai-onlinestore-shipping");
-		System.out.println("************* ******************" + obj.get("result"));
+		//System.out.println("************* ******************" + obj.get("result"));
 		return json;
 	}
 	
