bd81a92030b3a46102041437c1b970955b79eff1
Wala86 <benamorwala@yahoo.fr>
2017-08-22 11:41:21 +0200

    Update CustomerController.java

--- src/main/java/com/example/controllers/CustomerController.java
+++ src/main/java/com/example/controllers/CustomerController.java
@@ -1,31 +1,15 @@
 package com.example.controllers;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
-
-import javax.servlet.annotation.WebServlet;
 
 import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import ai.api.AIConfiguration;
-import ai.api.AIDataService;
-import ai.api.model.AIRequest;
-import ai.api.model.AIResponse;
-import ai.api.model.ResponseMessage;
-import javax.servlet.annotation.WebServlet;
-
-import ai.api.model.Fulfillment;
 
 @RestController
 public class CustomerController {
@@ -45,25 +29,20 @@ public class CustomerController {
 	 * json.put( "source", "apiai-onlinestore-shipping"); //
 	 * System.out.println("************* //
 	 * "+shippingZone+"******************"+action); // return json; // // } /*
-	 * 
-	 * @RequestMapping(value = "/webhook", method = RequestMethod.POST)
-	 * private @ResponseBody Map<String,Object> webhook(@RequestBody
-	 * Map<String,Object> obj) throws JSONException {
-	 * 
-	 * 
-	 * 
-	 * Map<String,Object> json = new HashMap<String,Object>();
-	 * System.out.println("************RequestBody*****************"+obj);
-	 * 
-	 * 
-	 * 
-	 * String Speech ="The cost of shipping to  is  for "+obj.get("result");
-	 * json.put("speech", " The cost of shipping to  is  1000 euros.");
-	 * json.put("displayText", " The cost of shipping to  is  1000 euros.");
-	 * json.put( "source", "apiai-onlinestore-shipping");
-	 * System.out.println("************* ******************"+obj.get("result"));
-	 * 
-	 * 
+	 */
+	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
+	private @ResponseBody Map<String, Object> webhook(@RequestBody Map<String, Object> obj) throws JSONException {
+
+		Map<String, Object> json = new HashMap<String, Object>();
+		System.out.println("************RequestBody*****************" + obj);
+
+		json.put("speech", " The cost of shipping to  is  1000 euros.");
+		json.put("displayText", " The cost of shipping to  is  1000 euros.");
+		json.put("source", "apiai-onlinestore-shipping");
+		System.out.println("************* ******************" + obj.get("result"));
+		return json;
+	}
+	/*
 	 * 
 	 * ResponseMessage speech = new ResponseMessage.Response();
 	 * speech.setSpeech(Arrays.asList("speech 1", "speech 2"));
@@ -81,7 +60,7 @@ public class CustomerController {
 	 * 
 	 * 
 	 * 
-	 * return json;
+	 *
 	 * 
 	 * }
 	 */
